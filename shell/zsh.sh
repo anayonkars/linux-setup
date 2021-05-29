8c0b4c70925c4b500cbdbf7b66be8c6fb9155cf4
Anayonkar Shivalkar <anayonkar.shivalkar@gmail.com>
2021-05-29 14:59:47 +0530

    modified zsh config script with -x

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
